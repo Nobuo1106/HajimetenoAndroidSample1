3b7900fe377ba0df0b0f0bfef599eff2568012fe
Nobuo1106 <gonnbe1986@gmail.com>
2023-08-02 13:45:41 +0900

    ログを仕込む

--- app/src/main/java/com/websarva/wings/android/hajimetenoandroid1/MainActivity.kt
+++ app/src/main/java/com/websarva/wings/android/hajimetenoandroid1/MainActivity.kt
@@ -1,8 +1,11 @@
 package com.websarva.wings.android.hajimetenoandroid1
 
 import android.os.Bundle
+import android.os.PersistableBundle
+import android.util.Log
 import android.view.View
 import android.widget.TextView
+import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
 import java.util.Date
 
@@ -16,5 +19,20 @@ class MainActivity : AppCompatActivity() {
     fun btnCurrentClick(v: View) {
        val txtResult = findViewById<TextView>(R.id.txtResult)
         txtResult.text = Date().toString()
+        Log.d("CurrentTime", Date().toString())
+        val toast = Toast.makeText(this, Date().toString(), Toast.LENGTH_LONG)
+        toast.show()
+    }
+
+    override fun onSaveInstanceState(outState: Bundle) {
+        super.onSaveInstanceState(outState)
+        val txtResult = findViewById<TextView>(R.id.txtResult)
+        outState.putString("txtResult", txtResult.text.toString())
+    }
+
+    override fun onRestoreInstanceState (savedInstanceState: Bundle){
+        super.onRestoreInstanceState(savedInstanceState)
+        val txtResult = findViewById<TextView>(R.id.txtResult)
+        txtResult.text = savedInstanceState.getString("txtResult")
     }
 }
