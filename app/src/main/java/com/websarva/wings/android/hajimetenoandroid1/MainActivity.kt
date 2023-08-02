cd5fb69b35842a45654e8c332553cd8f2b1c6bd1
Nobuo1106 <gonnbe1986@gmail.com>
2023-08-02 10:57:58 +0900

    done

--- app/src/main/java/com/websarva/wings/android/hajimetenoandroid1/MainActivity.kt
+++ app/src/main/java/com/websarva/wings/android/hajimetenoandroid1/MainActivity.kt
@@ -1,11 +1,20 @@
 package com.websarva.wings.android.hajimetenoandroid1
 
 import android.os.Bundle
+import android.view.View
+import android.widget.TextView
 import androidx.appcompat.app.AppCompatActivity
+import java.util.Date
+
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+    }
+
+    fun btnCurrentClick(v: View) {
+       val txtResult = findViewById<TextView>(R.id.txtResult)
+        txtResult.text = Date().toString()
     }
 }
