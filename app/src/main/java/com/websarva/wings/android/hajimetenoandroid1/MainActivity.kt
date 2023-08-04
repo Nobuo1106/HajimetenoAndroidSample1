f1d8d93f5014b77b2cfd663d6bb3fbc85a174a24
Nobuo1106 <gonnbe1986@gmail.com>
2023-08-04 18:47:28 +0900

    done

--- app/src/main/java/com/websarva/wings/android/hajimetenoandroid1/MainActivity.kt
+++ app/src/main/java/com/websarva/wings/android/hajimetenoandroid1/MainActivity.kt
@@ -1,38 +1,21 @@
 package com.websarva.wings.android.hajimetenoandroid1
 
 import android.os.Bundle
-import android.os.PersistableBundle
-import android.util.Log
-import android.view.View
+import android.widget.Button
+import android.widget.EditText
 import android.widget.TextView
-import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
-import java.util.Date
-
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
-    }
 
-    fun btnCurrentClick(v: View) {
-       val txtResult = findViewById<TextView>(R.id.txtResult)
-        txtResult.text = Date().toString()
-        Log.d("CurrentTime", Date().toString())
-        val toast = Toast.makeText(this, Date().toString(), Toast.LENGTH_LONG)
-        toast.show()
-    }
-
-    override fun onSaveInstanceState(outState: Bundle) {
-        super.onSaveInstanceState(outState)
+        val btnSend = findViewById<Button>(R.id.btnSend)
+        val txtName = findViewById<EditText>(R.id.txtName)
         val txtResult = findViewById<TextView>(R.id.txtResult)
-        outState.putString("txtResult", txtResult.text.toString())
-    }
-
-    override fun onRestoreInstanceState (savedInstanceState: Bundle){
-        super.onRestoreInstanceState(savedInstanceState)
-        val txtResult = findViewById<TextView>(R.id.txtResult)
-        txtResult.text = savedInstanceState.getString("txtResult")
+        btnSend.setOnClickListener {
+            txtResult.text = getString(R.string.greet, txtName.text)
+        }
     }
 }
