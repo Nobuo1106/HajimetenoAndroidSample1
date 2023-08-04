f6d298de7779232cb192b9d5d2886219d86d2e00
Nobuo1106 <gonnbe1986@gmail.com>
2023-08-04 22:10:21 +0900

    チェックボックス

--- app/src/main/java/com/websarva/wings/android/hajimetenoandroid1/MainActivity.kt
+++ app/src/main/java/com/websarva/wings/android/hajimetenoandroid1/MainActivity.kt
@@ -4,6 +4,7 @@ import android.os.Bundle
 import android.os.PersistableBundle
 import android.util.Log
 import android.view.View
+import android.widget.CheckBox
 import android.widget.TextView
 import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
@@ -14,25 +15,14 @@ class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
-    }
 
-    fun btnCurrentClick(v: View) {
-       val txtResult = findViewById<TextView>(R.id.txtResult)
-        txtResult.text = Date().toString()
-        Log.d("CurrentTime", Date().toString())
-        val toast = Toast.makeText(this, Date().toString(), Toast.LENGTH_LONG)
-        toast.show()
-    }
-
-    override fun onSaveInstanceState(outState: Bundle) {
-        super.onSaveInstanceState(outState)
-        val txtResult = findViewById<TextView>(R.id.txtResult)
-        outState.putString("txtResult", txtResult.text.toString())
-    }
-
-    override fun onRestoreInstanceState (savedInstanceState: Bundle){
-        super.onRestoreInstanceState(savedInstanceState)
-        val txtResult = findViewById<TextView>(R.id.txtResult)
-        txtResult.text = savedInstanceState.getString("txtResult")
+        val chk = findViewById<CheckBox>(R.id.chk)
+        chk.setOnCheckedChangeListener { _ , isChecked ->
+            Toast.makeText(
+                this@MainActivity,
+                if (isChecked) "メール送信をオン" else "メール送信をオフ",
+                Toast.LENGTH_SHORT
+            ).show()
+        }
     }
 }
