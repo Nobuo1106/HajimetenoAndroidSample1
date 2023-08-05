a23d8692bc1c71adaa12f800dec0022852bef033
Nobuo1106 <gonnbe1986@gmail.com>
2023-08-05 20:46:52 +0900

    done

--- app/src/main/java/com/websarva/wings/android/hajimetenoandroid1/MainActivity.kt
+++ app/src/main/java/com/websarva/wings/android/hajimetenoandroid1/MainActivity.kt
@@ -1,38 +1,28 @@
 package com.websarva.wings.android.hajimetenoandroid1
 
 import android.os.Bundle
-import android.os.PersistableBundle
-import android.util.Log
-import android.view.View
-import android.widget.TextView
-import android.widget.Toast
+import android.webkit.WebView
+import android.widget.Button
 import androidx.appcompat.app.AppCompatActivity
-import java.util.Date
-
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
-    }
 
-    fun btnCurrentClick(v: View) {
-       val txtResult = findViewById<TextView>(R.id.txtResult)
-        txtResult.text = Date().toString()
-        Log.d("CurrentTime", Date().toString())
-        val toast = Toast.makeText(this, Date().toString(), Toast.LENGTH_LONG)
-        toast.show()
-    }
+        val btnHome = findViewById<Button>(R.id.btnHome)
+        val btnEnv = findViewById<Button>(R.id.btnEnv)
+        val btnPub = findViewById<Button>(R.id.btnPub)
 
-    override fun onSaveInstanceState(outState: Bundle) {
-        super.onSaveInstanceState(outState)
-        val txtResult = findViewById<TextView>(R.id.txtResult)
-        outState.putString("txtResult", txtResult.text.toString())
-    }
-
-    override fun onRestoreInstanceState (savedInstanceState: Bundle){
-        super.onRestoreInstanceState(savedInstanceState)
-        val txtResult = findViewById<TextView>(R.id.txtResult)
-        txtResult.text = savedInstanceState.getString("txtResult")
+        val wv = findViewById<WebView>(R.id.wv)
+        btnHome.setOnClickListener {
+            wv.loadUrl("https://www.google.co.jp/")
+        }
+        btnEnv.setOnClickListener {
+            wv.loadUrl("https://qiita.com/")
+        }
+        btnPub.setOnClickListener {
+            wv.loadUrl("https://www.shuwasystem.co.jp/smp/")
+        }
     }
 }
