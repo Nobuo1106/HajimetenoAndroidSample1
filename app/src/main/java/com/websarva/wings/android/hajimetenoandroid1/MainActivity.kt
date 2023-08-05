9713a4d32f5474a718eb2a7310a6c1458b3d7e18
Nobuo1106 <gonnbe1986@gmail.com>
2023-08-05 12:41:03 +0900

    マイナス値を設定したシークバーを実装

--- app/src/main/java/com/websarva/wings/android/hajimetenoandroid1/MainActivity.kt
+++ app/src/main/java/com/websarva/wings/android/hajimetenoandroid1/MainActivity.kt
@@ -1,38 +1,29 @@
 package com.websarva.wings.android.hajimetenoandroid1
 
 import android.os.Bundle
-import android.os.PersistableBundle
-import android.util.Log
-import android.view.View
-import android.widget.TextView
+import android.widget.SeekBar
+import android.widget.SeekBar.OnSeekBarChangeListener
 import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
-import java.util.Date
-
+import java.util.Locale
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
-    }
 
-    fun btnCurrentClick(v: View) {
-       val txtResult = findViewById<TextView>(R.id.txtResult)
-        txtResult.text = Date().toString()
-        Log.d("CurrentTime", Date().toString())
-        val toast = Toast.makeText(this, Date().toString(), Toast.LENGTH_LONG)
-        toast.show()
-    }
-
-    override fun onSaveInstanceState(outState: Bundle) {
-        super.onSaveInstanceState(outState)
-        val txtResult = findViewById<TextView>(R.id.txtResult)
-        outState.putString("txtResult", txtResult.text.toString())
-    }
-
-    override fun onRestoreInstanceState (savedInstanceState: Bundle){
-        super.onRestoreInstanceState(savedInstanceState)
-        val txtResult = findViewById<TextView>(R.id.txtResult)
-        txtResult.text = savedInstanceState.getString("txtResult")
+        val seek = findViewById<SeekBar>(R.id.seek)
+        seek.setOnSeekBarChangeListener(object : OnSeekBarChangeListener {
+            override fun onProgressChanged(seekBar: SeekBar, progress: Int, fromUser: Boolean) {
+                val current = (progress - 10) * 10
+                Toast.makeText(
+                    this@MainActivity,
+                        String.format(Locale.JAPAN, "現在値：${current}"),
+                        Toast.LENGTH_SHORT
+                ).show()
+            }
+            override fun onStartTrackingTouch(seekBar: SeekBar) {}
+            override fun onStopTrackingTouch(seekBar: SeekBar) {}
+        })
     }
 }
